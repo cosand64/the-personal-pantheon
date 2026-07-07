c65668e2e957b11ea71c942c522f450921f49542
marcosdavis <macrosdavy@protonmail.com>
2026-07-07 09:39:35 -0600

    added a modal for confirming the form and message has been sent

--- src/js/contact-us.js
+++ src/js/contact-us.js
@@ -1,8 +1,47 @@
 import { searchRequest  } from "./navigation.mjs";
 import '../css/contact-us.css';
 
+const formBtn = document.querySelector("#submit-btn");
+const closeBtn = document.querySelector('.close-button');
+const modal = document.querySelector("#modal")
+const contactForm = document.querySelector("#contact-right-column > form")
+
+function displayConfirmationMessage() {
+    modal.classList.add('open');
+    modal.setAttribute('aria-hidden', false);
+}
+
+function closeConfirmationMessage() {
+    modal.classList.remove('open');
+    modal.setAttribute('aria-hidden', true);
+}
+
+function closeMessageAndResetForm() {
+    contactForm.reset();
+    closeConfirmationMessage();
+}
+
 function init() {
     searchRequest();
+
+    formBtn.addEventListener('click', e => {
+        e.preventDefault();
+        displayConfirmationMessage();
+    });
+
+    closeBtn.addEventListener('click', closeMessageAndResetForm);
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            closeMessageAndResetForm();
+        }
+    })
+
+    window.addEventListener('click', e => {
+        if (e.target === modal) {
+            closeMessageAndResetForm();
+        }
+    })
 }
 
 init();
